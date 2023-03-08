af7093351e54de97b8a84d3f2b94b5aa0e5e9117
BigFoot <zan04259@naver.com>
2023-03-08 16:50:22 +0900

    conf: 초기 간단한 설정

---
+++ BackFinalPriject/src/main/java/com/example/backfinalpriject/BackFinalPrijectApplication.java
@@ -0,0 +1,13 @@
+package com.example.backfinalpriject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFinalPrijectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFinalPrijectApplication.class, args);
+	}
+
+}
