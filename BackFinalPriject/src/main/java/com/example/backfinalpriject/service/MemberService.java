9c5e9cf4a35609c092d42f3019f801fd85f138f1
BigFoot <zan04259@naver.com>
2023-03-21 14:30:14 +0900

    feat: strategy, strategyVideo entity 구별

--- BackFinalPriject/src/main/java/com/example/backfinalpriject/service/MemberService.java
+++ BackFinalPriject/src/main/java/com/example/backfinalpriject/service/MemberService.java
@@ -1,58 +1,14 @@
 package com.example.backfinalpriject.service;
 
-import com.example.backfinalpriject.dto.request.MemberSignupRequest;
-import com.example.backfinalpriject.dto.response.MemberLoginResponse;
-import com.example.backfinalpriject.entity.Member;
-import com.example.backfinalpriject.repository.MemberRepository;
 import com.example.backfinalpriject.dto.request.MemberLoginRequest;
-import com.example.backfinalpriject.exception.ErrorCode;
-import com.example.backfinalpriject.exception.GlobalException;
-import lombok.RequiredArgsConstructor;
-import org.hibernate.Session;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.example.backfinalpriject.dto.request.MemberSignupRequest;
 
-@Service
-@RequiredArgsConstructor
-@Transactional
-public class MemberService {
+public interface MemberService {
+    void login(MemberLoginRequest request);
 
-    private final MemberRepository memberRepository;
-    private final PasswordEncoder encoder;
+    void signup(MemberSignupRequest request);
 
-    public void login(MemberLoginRequest request){
-        Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(()
-                -> new GlobalException(ErrorCode.USER_NOT_FOUND, "해당 아이디가 존재하지 않습니다"));
-        if (!encoder.matches(request.getPassword(), member.getPassword())){
-            throw new GlobalException(ErrorCode.INVALID_PASSWORD, "비밀번호가 일치하지 않습니다");
-        }
-    }
+    void adminSignup(MemberSignupRequest request);
 
-    public void signup(MemberSignupRequest request){
-        memberRepository.findByEmail(request.getEmail()).ifPresent(x -> {
-            throw new GlobalException(ErrorCode.DUPLICATED_USER_EMAIL, request.getEmail()+" 는 이미 존재하는 아이디입니다");
-            });
-        String encryptedPwd = encoder.encode(request.getPassword());
-        memberRepository.save(Member.createUser(request, encryptedPwd));
-    }
-
-    public void adminSignup(MemberSignupRequest request){
-        memberRepository.findByEmail(request.getEmail()).ifPresent(x -> {
-            throw new GlobalException(ErrorCode.DUPLICATED_USER_EMAIL, request.getEmail()+" 는 이미 존재하는 아이디입니다.");
-        });
-        String encryptedPwd = encoder.encode(request.getPassword());
-        memberRepository.save(Member.createAdmin(request, encryptedPwd));
-    }
-
-    public void adminLogin(MemberLoginRequest request){
-        Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(()
-                -> new GlobalException(ErrorCode.USER_NOT_FOUND, "해당 아이디가 존재하지 않습니다"));
-        if (!encoder.matches(request.getPassword(), member.getPassword())){
-            throw new GlobalException(ErrorCode.INVALID_PASSWORD, "비밀번호가 일치하지 않습니다");
-        }
-        if (member.getRole() != 1){
-            throw new GlobalException(ErrorCode.INVALID_PERMISSION, "관리자 외에 접근이 불가합니다.");
-        }
-    }
+    void adminLogin(MemberLoginRequest request);
 }
