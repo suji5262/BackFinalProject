59cbf9fce4d7a7289ae4afce92540d09aae9307c
BigFoot <zan04259@naver.com>
2023-03-23 20:50:11 +0900

    feat: 학습전략 비디오 올리기 기능 구현

--- BackFinalPriject/src/main/java/com/example/backfinalpriject/admin/strategy/service/StrategyVideoService.java
+++ BackFinalPriject/src/main/java/com/example/backfinalpriject/admin/strategy/service/StrategyVideoService.java
@@ -1,4 +1,9 @@
 package com.example.backfinalpriject.admin.strategy.service;
 
+import com.example.backfinalpriject.admin.strategy.dto.request.StrategyVideoRequest;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface StrategyVideoService {
+
+    String insertStrategyVideo(MultipartFile file, StrategyVideoRequest videoRequest);
 }
