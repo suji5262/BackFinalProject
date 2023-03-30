5c2c1b173f38e439ee48b6cfbaabaf5240431c09
suji5262 <suji5262@gmail.com>
2023-03-30 19:16:02 +0900

    Feat: StrategyService 추가 - 전체조회 추가 - 상세조회 추가

--- BackFinalPriject/src/main/java/com/example/backfinalpriject/admin/strategy/service/StrategyService.java
+++ BackFinalPriject/src/main/java/com/example/backfinalpriject/admin/strategy/service/StrategyService.java
@@ -1,11 +1,18 @@
 package com.example.backfinalpriject.admin.strategy.service;
 
 import com.example.backfinalpriject.admin.strategy.dto.request.StrategyRequest;
+import com.example.backfinalpriject.admin.strategy.dto.response.StrategyDetailPageResponse;
+import com.example.backfinalpriject.admin.strategy.dto.response.StrategyPageResponse;
 import com.example.backfinalpriject.distinction.entity.Subject;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface StrategyService {
 
     String strategyBoard(MultipartFile file,StrategyRequest strategyRequest);
 
+    List<StrategyPageResponse> getStrategyPageList(); // 전체조회
+
+    StrategyDetailPageResponse selectDetailStrategy(Long id); // 상세조회
 }
