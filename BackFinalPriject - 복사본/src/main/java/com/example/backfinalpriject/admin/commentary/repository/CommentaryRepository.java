e431b50fcc05236f0935881d05ebf683c9af580d
Ryusun <ryusunshine@gmail.com>
2023-04-06 15:18:18 +0900

    Delete BackFinalPriject - 복사본/src/main/java/com/example/backfinalpriject/admin/commentary directory

--- BackFinalPriject - 복사본/src/main/java/com/example/backfinalpriject/admin/commentary/repository/CommentaryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backfinalpriject.admin.commentary.repository;
-
-import com.example.backfinalpriject.admin.commentary.entity.Commentary;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentaryRepository extends JpaRepository<Commentary, Long> {
-
-}
